04d63cd518fc9edd7694edca5e892841a7d175e2
Francois Aubertin <francois.aubertin@gmail.com>
2015-04-21 16:45:07 +0000

    test

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://localhost/pubquizz-dev',
+	db: 'mongodb://pubquizz:dbpassword@ds039281.mongolab.com:39281/heroku_app34546832',
 	app: {
 		title: 'pubquizz - Development Environment'
 	},
